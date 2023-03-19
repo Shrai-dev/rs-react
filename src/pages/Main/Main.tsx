2facc0451f46ce89f8cc616fdef8ac619ab43bd8
Anastasia <nastia.shrai@gmail.com>
2023-03-19 20:02:53 +0200

    feat: add rendering cards to main page and styles

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,34 @@
+import SearchBar from '../../components/SearchBar/SearchBar';
 import React, { Component } from 'react';
+import { data } from '../../utils/data';
+import Card from '../../components/Card/Card';
+import './Main.scss';
 
 export default class Main extends Component {
   render() {
+    const cards = data.map((item) => {
+      return (
+        <Card
+          key={item.id}
+          title={item.title}
+          brand={item.brand}
+          category={item.category}
+          description={item.description}
+          discountPercentage={0}
+          id={item.id}
+          images={item.images}
+          price={item.price}
+          rating={item.rating}
+          stock={item.stock}
+          thumbnail={item.thumbnail}
+        />
+      );
+    });
     return (
-      <div>
-        <h2>Main</h2>
-      </div>
+      <main className="main">
+        <SearchBar />
+        <div className="cards__wrapper">{cards}</div>
+      </main>
     );
   }
 }
