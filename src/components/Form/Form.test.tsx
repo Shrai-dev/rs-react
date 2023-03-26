784f2b3d5a547d28f8ff75c3aac22a9b803e52b0
Anastasia <nastia.shrai@gmail.com>
2023-03-26 21:26:26 +0300

    feat: add tests to check validation

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -4,6 +4,16 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Form from './Form';
 
+const mockData = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  birthday: '2004-05-06',
+  country: 'Ukraine',
+  promo: 'Yes',
+  gender: 'Female',
+  file: 'photo.png',
+};
+
 describe('Form', () => {
   it('Render Form'),
     () => {
@@ -46,19 +56,14 @@ describe('Form', () => {
 
       expect(country).toBeInTheDocument();
     };
-  it('Render Form elements: privacy-policy checkbox'),
-    () => {
-      render(<Form />);
-      const privacyPolicy = screen.getByTestId('privacy-policy');
-
-      expect(privacyPolicy).toBeInTheDocument();
-    };
   it('Render Form elements: promo-notifications checkbox'),
     () => {
       render(<Form />);
       const promoNotifications = screen.getByTestId('promo-notifications');
-
       expect(promoNotifications).toBeInTheDocument();
+
+      userEvent.click(promoNotifications);
+      expect(promoNotifications).toHaveAttribute('checked', 'true');
     };
   it('Render Form elements: radio buttons'),
     () => {
@@ -82,5 +87,74 @@ describe('Form', () => {
       const submitButton = screen.getByTestId('submit-btn');
 
       expect(submitButton).toBeInTheDocument();
+    };
+  it('Check Form elements validation and shows error messages: empty form'),
+    () => {
+      render(<Form />);
+
+      const firstNameError = screen.getByTestId('first-name-error');
+      const lastNameError = screen.getByTestId('last-name-error');
+      const birthdayError = screen.getByTestId('birthday-error');
+      const countryError = screen.getByTestId('country-error');
+      const promoError = screen.getByTestId('promo-error');
+      const genderError = screen.getByTestId('gender-error');
+      const fileUploadError = screen.getByTestId('file-upload-error');
+      const submitButton = screen.getByTestId('submit-btn');
+
+      userEvent.click(submitButton);
+
+      expect(firstNameError).toHaveValue('The first name is required');
+      expect(lastNameError).toHaveValue('The last name is required');
+      expect(birthdayError).toHaveValue('The birthday is required');
+      expect(countryError).toHaveValue('The country is required');
+      expect(promoError).toHaveValue('The field is required');
+      expect(genderError).toHaveValue('The gender is required');
+      expect(fileUploadError).toHaveValue('The first name is required');
+    };
+  it('Check Form elements validation and shows error message: first name input'),
+    () => {
+      render(<Form />);
+      const firstName = screen.getByTestId('first-name');
+      const firstNameError = screen.getByTestId('first-name-error');
+      const submitButton = screen.getByTestId('submit-btn');
+
+      userEvent.type(firstName, 'abc');
+      userEvent.click(submitButton);
+
+      expect(firstNameError).toHaveValue('The first name should start from capital letter');
+    };
+  it('Check Form elements validation and shows error message: last name input'),
+    () => {
+      render(<Form />);
+      const lastName = screen.getByTestId('last-name');
+      const lastNameError = screen.getByTestId('last-name-error');
+      const submitButton = screen.getByTestId('submit-btn');
+
+      userEvent.type(lastName, 'abc');
+      userEvent.click(submitButton);
+      expect(lastNameError).toHaveValue('The last name should start from capital letter');
+    };
+  it('Check Form elements validation and shows alert message'),
+    () => {
+      render(<Form />);
+      const firstName = screen.getByTestId('first-name');
+      const lastName = screen.getByTestId('last-name');
+      const birthday = screen.getByTestId('birthday');
+      const country = screen.getByTestId('country');
+      const promo = screen.getByTestId('promo');
+      const gender = screen.getByTestId('gender');
+      const fileUpload = screen.getByTestId('file-upload');
+      const submitButton = screen.getByTestId('submit-btn');
+
+      userEvent.type(firstName, `${mockData.firstName}`);
+      userEvent.type(lastName, `${mockData.lastName}`);
+      userEvent.type(birthday, `${mockData.birthday}`);
+      userEvent.type(country, `${mockData.country}`);
+      userEvent.type(promo, `${mockData.promo}`);
+      userEvent.type(gender, `${mockData.gender}`);
+      userEvent.type(fileUpload, `${mockData.file}`);
+
+      userEvent.click(submitButton);
+      expect(window.alert).toHaveBeenCalledWith('Thank you! The data has been saved');
     };
 });
