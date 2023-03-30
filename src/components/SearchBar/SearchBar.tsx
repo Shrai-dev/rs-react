f28fd3ae098656926b44eba2815ab443d4d465b4
Anastasia <nastia.shrai@gmail.com>
2023-03-30 23:05:26 +0300

    feat: change to functional component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,60 +1,41 @@
-import React, { ChangeEvent, Component } from 'react';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import './SearchBar.scss';
 
-export default class SearchBar extends Component {
-  constructor(props: Record<string, never> | Readonly<Record<string, never>>) {
-    super(props);
+const SearchBar: FC = () => {
+  const [searchQuery, setSearchQuery] = useState(localStorage.getItem('searchQuery') || '');
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
-  state = JSON.parse(localStorage.getItem('formData') || '{}');
-
-  componentDidMount(): void {
+  useEffect(() => {
     if (localStorage.getItem('formData') !== null) {
-      this.setState(this.state);
+      setSearchQuery(searchQuery);
     }
-  }
+  }, [searchQuery]);
 
-  componentDidUpdate(): void {
-    this.setLocalStorage();
-  }
-
-  componentWillUnmount(): void {
-    this.setLocalStorage();
-  }
-
-  handleChange(e: ChangeEvent<HTMLInputElement>) {
-    this.setState({ searchValue: e.target.value });
-    this.setLocalStorage();
-  }
-
-  handleFormSubmit(e: { preventDefault: () => void }): void {
+  const handleFormSubmit = (e: { preventDefault: () => void }): void => {
     e.preventDefault();
-    this.setLocalStorage();
-  }
+    setLocalStorage();
+  };
 
-  setLocalStorage(): void {
-    localStorage.setItem('formData', JSON.stringify(this.state));
-  }
+  const setLocalStorage = (): void => {
+    localStorage.setItem('searchQuery', searchQuery);
+  };
 
-  render() {
-    return (
-      <div className="search__container">
-        <form onSubmit={this.handleFormSubmit}>
-          <input
-            className="search__input"
-            type="search"
-            name="searchValue"
-            aria-label="search"
-            value={this.state.searchValue}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => this.handleChange(e)}
-          />
-          <button className="search__btn" type="submit">
-            Search
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search__container" data-testid="search-container">
+      <form onSubmit={handleFormSubmit}>
+        <input
+          className="search__input"
+          type="search"
+          name="searchValue"
+          aria-label="search"
+          value={searchQuery}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)}
+        />
+        <button className="search__btn" type="submit" data-testid="search-btn">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
