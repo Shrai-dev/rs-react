5a392923d52f6275251bd803b41ee196a7bbd28a
Anastasia <nastia.shrai@gmail.com>
2023-04-11 22:49:29 +0300

    fix: change Main page and styles

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,19 +1,11 @@
-import SearchBar from '../../components/SearchBar/SearchBar';
 import React, { FC } from 'react';
-import { data } from '../../utils/data';
-import Card from '../../components/Card/Card';
+import SearchBar from '../../components/SearchBar/SearchBar';
 import './Main.scss';
 
 const Main: FC = () => {
-  const cards = data.map((item) => {
-    return <Card key={item.id} card={item} />;
-  });
   return (
     <main className="main">
       <SearchBar />
-      <div className="cards__wrapper" data-testid="cards-wrapper">
-        {cards}
-      </div>
     </main>
   );
 };
