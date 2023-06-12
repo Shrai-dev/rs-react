43967f0711281ca345470a7cd690b1b24577132e
Anastasia <nastia.shrai@gmail.com>
2023-06-12 22:27:04 +0300

    refactor: refactor CardList component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,16 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Card from '../../components/Card/Card';
 import { MoviesProps } from 'utils/types';
 import './CardList.scss';
+import Popup from '../../components/Popup/Popup';
 
 const CardList: FC<MoviesProps> = ({ movies }) => {
+  const [popupActive, setPopupActive] = useState(false);
+  const [movieId, setMovieId] = useState('');
   const cards = movies.map((item) => {
-    return <Card key={item.id} {...item} />;
+    return (
+      <Card
+        handleClick={() => {
+          setPopupActive(true);
+          setMovieId(item.id.toString());
+        }}
+        key={item.id}
+        {...item}
+      />
+    );
   });
   return (
-    <section className="cards__wrapper" data-testid="cards-wrapper">
-      {cards}
-    </section>
+    <>
+      <section className="cards__wrapper" data-testid="cards-wrapper">
+        {cards}
+      </section>
+      <Popup active={popupActive} setActive={setPopupActive} movieId={movieId} />
+    </>
   );
 };
 
