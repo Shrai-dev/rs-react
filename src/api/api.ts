a4508849e875c646bd238ebafbec15f8092adbeb
Anastasia <nastia.shrai@gmail.com>
2023-06-12 22:20:43 +0300

    feat: add functions for handling api calls

--- src/api/api.ts
+++ src/api/api.ts
@@ -16,3 +16,21 @@ export const showPopularMovies = async () => {
   const data = await res.json();
   return data.results;
 };
+
+export const getAllGenres = async () => {
+  const urlGenres = `https://api.themoviedb.org/3/genre/movie/list?api_key=1522de262832f8a37e8d96e7ed928ba5&language=en-US`;
+  try {
+    const resGenres = await fetch(urlGenres);
+    const dataGenres = await resGenres.json();
+    return dataGenres;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchMovieById = async (movieId: string) => {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+};
